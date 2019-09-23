864ee03b838f3482cfc2d88977eb4773ccfcc7d9
Pablo <borrazaspablo@gmail.com>
2019-09-23 17:44:51 -0300

    add app shell, router with post and page-not-found pages and material dependencies

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'posts', loadChildren: () => import(`./posts/posts.module`).then(m => m.PostsModule) },
+  { path: 'post/:id', loadChildren: () => import(`./posts/posts.module`).then(m => m.PostsModule) },
+  { path: '', redirectTo: '/posts', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AppMaterialModule} from './app-material.module';
+import {PageNotFoundComponent } from './page-not-found/page-not-found.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AppMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
